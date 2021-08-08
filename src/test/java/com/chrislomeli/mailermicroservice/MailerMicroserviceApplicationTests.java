6ecf1753de1c5e14672a9b6a6149c247f297ab40
chris lomeli <christopher.john.lomeli>
2021-08-08 06:23:56 -0700

    init

---
+++ src/test/java/com/chrislomeli/mailermicroservice/MailerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrislomeli.mailermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
